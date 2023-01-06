686f098460a8225a339cbc83b0e070331b6ec903
Jonathan Delgado <hi@jon.soy>
2023-01-06 01:05:07 -0300

    build: add support prettier

--- tests/envuse.test.ts
+++ tests/envuse.test.ts
@@ -1,17 +1,27 @@
-import { describe, expect, it } from "vitest"
-import { readFile } from "node:fs/promises"
-import { existsSync } from "node:fs"
-import { createDTSFile, createProgramFromFile, createStoreTypeReference, parseAsync } from "../envuse.mjs"
-import { FieldDTSFile, InterfaceDTSFile, PrimitiveNumberTypeDTSFile, PrimitiveStringTypeDTSFile, UnionDTSFile } from "../utils/dtsfile.mjs"
+import { describe, expect, it } from "vitest";
+import { readFile } from "node:fs/promises";
+import { existsSync } from "node:fs";
+import {
+  createDTSFile,
+  createProgramFromFile,
+  createStoreTypeReference,
+  parseAsync,
+} from "../envuse.mjs";
+import {
+  FieldDTSFile,
+  InterfaceDTSFile,
+  PrimitiveNumberTypeDTSFile,
+  PrimitiveStringTypeDTSFile,
+  UnionDTSFile,
+} from "../utils/dtsfile.mjs";
 
 describe("Sample 1", () => {
   it("should prepare envuse file", async () => {
-    const testDirname = new URL("_sample-1/", import.meta.url)
-    const envuseFile = new URL(".envuse", testDirname)
+    const testDirname = new URL("_sample-1/", import.meta.url);
+    const envuseFile = new URL(".envuse", testDirname);
 
     expect(
-      createDTSFile(await createProgramFromFile(envuseFile), "demo")
-        .toString()
+      createDTSFile(await createProgramFromFile(envuseFile), "demo").toString()
     ).toMatchInlineSnapshot(`
       "interface demo {
         AAA: any
@@ -20,39 +30,57 @@ describe("Sample 1", () => {
         BAZ: string
         PORT: number
       }"
-    `)
-  })
-})
-
+    `);
+  });
+});
 
 describe("Sample 2: store of types", () => {
   it("create store type reference", async () => {
-    const testDirname = new URL("_sample-2/", import.meta.url)
-    const definitionURL = new URL("def.json", testDirname)
-    const typesURL = new URL("types.ts", testDirname)
-    const envuse1File = new URL(".envuse1", testDirname)
-    const envuse2File = new URL(".envuse2", testDirname)
+    const testDirname = new URL("_sample-2/", import.meta.url);
+    const definitionURL = new URL("def.json", testDirname);
+    const typesURL = new URL("types.ts", testDirname);
+    const envuse1File = new URL(".envuse1", testDirname);
+    const envuse2File = new URL(".envuse2", testDirname);
 
-    const storeTypeReference = createStoreTypeReference(definitionURL, typesURL)
+    const storeTypeReference = createStoreTypeReference(
+      definitionURL,
+      typesURL
+    );
 
-    expect(storeTypeReference).toBeTypeOf("object")
-    expect(storeTypeReference).not.toBeNull()
+    expect(storeTypeReference).toBeTypeOf("object");
+    expect(storeTypeReference).not.toBeNull();
 
-    await storeTypeReference.pull()
+    await storeTypeReference.pull();
 
-    storeTypeReference.attachInterfaceDTSFile(new InterfaceDTSFile("a", [
-      new FieldDTSFile("name", new PrimitiveStringTypeDTSFile()),
-      new FieldDTSFile("age", new UnionDTSFile([new PrimitiveStringTypeDTSFile(), new PrimitiveNumberTypeDTSFile()])),
-    ]))
-    storeTypeReference.attachInterfaceDTSFile(new InterfaceDTSFile("c", []))
-    storeTypeReference.attachInterfaceDTSFile(new InterfaceDTSFile(".envuse", []))
-    storeTypeReference.attachInterfaceDTSFile(new InterfaceDTSFile(".envuse.prod", []))
-    storeTypeReference.attachInterfaceDTSFile(new InterfaceDTSFile(".envuse/dev.env1", []))
-    storeTypeReference.attachInterfaceDTSFile(new InterfaceDTSFile(".envuse/prod.env", []))
+    storeTypeReference.attachInterfaceDTSFile(
+      new InterfaceDTSFile("a", [
+        new FieldDTSFile("name", new PrimitiveStringTypeDTSFile()),
+        new FieldDTSFile(
+          "age",
+          new UnionDTSFile([
+            new PrimitiveStringTypeDTSFile(),
+            new PrimitiveNumberTypeDTSFile(),
+          ])
+        ),
+      ])
+    );
+    storeTypeReference.attachInterfaceDTSFile(new InterfaceDTSFile("c", []));
+    storeTypeReference.attachInterfaceDTSFile(
+      new InterfaceDTSFile(".envuse", [])
+    );
+    storeTypeReference.attachInterfaceDTSFile(
+      new InterfaceDTSFile(".envuse.prod", [])
+    );
+    storeTypeReference.attachInterfaceDTSFile(
+      new InterfaceDTSFile(".envuse/dev.env1", [])
+    );
+    storeTypeReference.attachInterfaceDTSFile(
+      new InterfaceDTSFile(".envuse/prod.env", [])
+    );
 
-    await storeTypeReference.sync()
+    await storeTypeReference.sync();
 
-    expect(await readFile(definitionURL, 'utf-8')).toMatchInlineSnapshot(`
+    expect(await readFile(definitionURL, "utf-8")).toMatchInlineSnapshot(`
       "[
         [
           \\"a\\",
@@ -83,8 +111,8 @@ describe("Sample 2: store of types", () => {
           \\"interface _envuse_prod_env {\\\\n}\\"
         ]
       ]"
-    `)
-    expect(await readFile(typesURL, 'utf-8')).toMatchInlineSnapshot(`
+    `);
+    expect(await readFile(typesURL, "utf-8")).toMatchInlineSnapshot(`
       "interface a {
         name: string
         age: string
@@ -119,14 +147,6 @@ describe("Sample 2: store of types", () => {
         \\".envuse/dev.env1\\": _envuse_dev_env1
         \\".envuse/prod.env\\": _envuse_prod_env
       }"
-    `)
-  })
-})
-
-it("Should parser file", async () => {
-  const testDirname = new URL("_sample-3/", import.meta.url)
-  const definitionURL = new URL("def.json", testDirname)
-
-  const a = await parseAsync("sample.envuse")
-})
-
+    `);
+  });
+});
