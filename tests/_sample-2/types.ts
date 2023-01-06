1b336b33ea501f1665ffa815f393798b59259d5b
Jonathan Delgado <hi@jon.soy>
2023-01-06 01:37:01 -0300

    test: upgrade format files

--- tests/_sample-2/types.ts
+++ tests/_sample-2/types.ts
@@ -1,26 +1,34 @@
 interface a {
-  name: string;
-  age: string | number;
+  name: string
+  age: string
+  | number
 }
 
-interface b {}
+interface b {
+}
 
-interface c {}
+interface c {
+}
 
-interface _envuse {}
+interface _envuse {
+}
 
-interface _envuse_prod {}
+interface _envuse_prod {
+}
 
-interface _envuse_dev_env1 {}
+interface _envuse_dev_env1 {
+}
 
-interface _envuse_prod_env {}
+interface _envuse_prod_env {
+}
+
 
 export interface MapParsers {
-  a: a;
-  b: b;
-  c: c;
-  ".envuse": _envuse;
-  ".envuse.prod": _envuse_prod;
-  ".envuse/dev.env1": _envuse_dev_env1;
-  ".envuse/prod.env": _envuse_prod_env;
+  a: a
+  b: b
+  c: c
+  ".envuse": _envuse
+  ".envuse.prod": _envuse_prod
+  ".envuse/dev.env1": _envuse_dev_env1
+  ".envuse/prod.env": _envuse_prod_env
 }
