5daf9685c21f6751612025ed0c651a19ba8338e5
Jonathan Delgado <hi@jon.soy>
2023-01-21 18:55:31 -0300

    refactor: define dist folder

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,38 @@
 import { RollupOptions } from "rollup";
 import tsc from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
-const options: RollupOptions = {
-  input: "./src/envuse.mts",
-  plugins: [tsc()],
-  output: [
-    {
-      file: "cjs/envuse.cjs",
-      sourcemap: true,
-      format: "cjs",
+const options: RollupOptions[] = [
+  {
+    input: "./src/envuse.mts",
+    plugins: [dts()],
+    output: {
+      file: "dist/envuse.d.ts",
+      format: "es",
     },
-    {
-      file: "cjs/envuse.js",
-      sourcemap: true,
-      format: "cjs",
-    },
-  ],
-  external: ["node:fs/promises", "node:fs", "node:process", "@envuse/wasm"],
-};
+  },
+  {
+    input: "./src/envuse.mts",
+    plugins: [tsc()],
+    output: [
+      {
+        file: "dist/envuse.cjs",
+        sourcemap: true,
+        format: "cjs",
+      },
+      {
+        file: "dist/envuse.js",
+        sourcemap: true,
+        format: "cjs",
+      },
+      {
+        file: "dist/envuse.mjs",
+        sourcemap: true,
+        format: "esm",
+      },
+    ],
+    external: ["node:fs/promises", "node:fs", "node:process", "@envuse/wasm"],
+  },
+];
 
 export default options;
